e652c0943e2f7a3275fd6848ea8d2d50a409dbd9
Betti83771 <akylu8@gmail.com>
2023-02-13 16:07:47 +0100

    added panel for ref drv

--- ui.py
+++ ui.py
@@ -1,7 +1,23 @@
 import bpy
 
+class OtherOperatorsPanel(bpy.types.Panel):
+    """Subpanel of the main MAD Shading tool, with additional operators"""
+    bl_label = "Other Tools"
+    bl_idname = "OBJECT_PT_otheroperators"
+    bl_space_type = 'NODE_EDITOR'
+    bl_category = "MAD Shading Tools"
+    bl_region_type = 'UI'
+    bl_parent_id = "OBJECT_PT_noderig"
+    def draw(self, context):
+        layout = self.layout
+        row = layout.row()
+        row.operator("object.refresh_drivers")
+        row = layout.row()
+        row.operator("object.remobe_broken_drivers")
+
 classes = [
     # operator classes here
+
 ]
 
 def ui_register():
